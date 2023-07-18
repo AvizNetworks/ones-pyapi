4f6081f266524ba830669a1e107e0e63767e9bfa
KavyanshAviz <kavyansh.pandey@aviznetworks.com>
2023-07-18 22:34:30 +0530

    Added copyright description in .py files

--- restclient/telemetry/client.py
+++ restclient/telemetry/client.py
@@ -1,3 +1,26 @@
+"""
+Copyright (c) 2023, Aviz Networks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
 # Importing neccessery modules
 import logging
 import json
@@ -9,11 +32,6 @@ from .endpoints import *
 from .excpetions import OnesClientExpection
 from .constants import *
 from .util import request_handler, request_handler_with_payload, request_handler_with_query_params
-
-
-# Loading Environment Variables
-# from dotenv import load_dotenv
-# load_dotenv()
 
 
 class ONESClient(object):
